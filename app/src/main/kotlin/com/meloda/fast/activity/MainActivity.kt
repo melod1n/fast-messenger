ebba43fc70a661f5df23ad978007e68884ab052e
Danil Nikolaev <lischenkodev@gmail.com>
2021-09-27 17:06:20 +0300

    Update MainActivity.kt

--- app/src/main/kotlin/com/meloda/fast/activity/MainActivity.kt
+++ app/src/main/kotlin/com/meloda/fast/activity/MainActivity.kt
@@ -1,24 +1,8 @@
 package com.meloda.fast.activity
 
-import android.os.Bundle
-import android.viewbinding.library.activity.viewBinding
-import androidx.lifecycle.lifecycleScope
 import com.meloda.fast.R
 import com.meloda.fast.base.BaseActivity
-import com.meloda.fast.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : BaseActivity(R.layout.activity_main) {
-
-    private val binding: ActivityMainBinding by viewBinding()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        lifecycleScope.launchWhenStarted {
-
-        }
-    }
-
-}
+class MainActivity : BaseActivity(R.layout.activity_main)
