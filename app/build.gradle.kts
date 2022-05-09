38b2e6effd8d498caf274018b4d879daea774570
Danil Nikolaev <lischenkodev@gmail.com>
2022-05-09 18:36:43 +0300

    * misc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ val otaSecretCode: String = gradleLocalProperties(rootDir).getProperty("otaSecre
 
 val majorVersion = 1
 val minorVersion = 5
-val patchVersion = 2
+val patchVersion = 3
 
 plugins {
     id("com.android.application")
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("com.android.tools.build:gradle:7.1.3")
 
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.1")
     }
 }
 
