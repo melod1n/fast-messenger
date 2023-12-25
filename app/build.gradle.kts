030efdf738df02e628db623d4ce1d17388aa3b42
Danil Nikolaev <lischenkodev@gmail.com>
2023-12-25 18:48:20 +0300

    added keystore, keystore properties, update build.gradle.kts and android workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 import com.android.build.api.variant.BuildConfigField
-import com.android.build.gradle.internal.api.BaseVariantOutputImpl
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+import java.util.Properties
 
 val sdkPackage: String = getLocalProperty("sdkPackage", "\"\"")
 val sdkFingerprint: String = getLocalProperty("sdkFingerprint", "\"\"")
@@ -133,11 +133,39 @@ android {
         resourceConfigurations += listOf("en", "ru")
     }
 
+    signingConfigs {
+        create("release") {
+            val keystoreProperties = Properties()
+            val keystorePropertiesFile = file("keystore/keystore.properties")
+
+            storeFile = file("keystore/keystore.jks")
+
+            if (keystorePropertiesFile.exists()) {
+                keystorePropertiesFile.inputStream().let(keystoreProperties::load)
+                storePassword = keystoreProperties.getProperty("storePassword")
+                keyAlias = keystoreProperties.getProperty("keyAlias")
+                keyPassword = keystoreProperties.getProperty("keyPassword")
+            } else {
+                storePassword = System.getenv("KEYSTORE_PASSWORD")
+                keyAlias = System.getenv("RELEASE_SIGN_KEY_ALIAS")
+                keyPassword = System.getenv("RELEASE_SIGN_KEY_PASSWORD")
+            }
+        }
+
+        create("debugSigning") {
+            initWith(getByName("release"))
+        }
+    }
+
     buildTypes {
         getByName("debug") {
+            signingConfig = signingConfigs.getByName("debugSigning")
+
             versionNameSuffix = "_${getVersionName()}"
         }
         getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+
             isMinifyEnabled = false
 
             proguardFiles(
